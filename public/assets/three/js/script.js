7e189fa2d1e87bdf25bd70599eea63c9be9b0fec
Herotik <alex.mks6@gmail.com>
2019-09-25 17:44:51 +0200

    ajout page test

--- public/assets/three/js/script.js
+++ public/assets/three/js/script.js
@@ -1,12 +1,13 @@
-window.onload = init;
-console.ward = function() {}; // what warnings?
+<script id="rendered-js">
+      window.onload = init;
+console.ward = function () {}; // what warnings?
 
 function init() {
   var root = new THREERoot({
     createCameraControls: !true,
-    antialias: (window.devicePixelRatio === 1),
-    fov: 80
-  });
+    antialias: window.devicePixelRatio === 1,
+    fov: 80 });
+
 
   root.renderer.setClearColor(0x000000, 0);
   root.renderer.setPixelRatio(window.devicePixelRatio || 1);
@@ -16,30 +17,30 @@ function init() {
   var height = 60;
 
   var slide = new Slide(width, height, 'out');
-	var l1 = new THREE.ImageLoader();
-	l1.setCrossOrigin('Anonymous');
-	l1.load('../../../ressources/images/drake.png', function(img) {
-	  slide.setImage(img);
-	})
+  var l1 = new THREE.ImageLoader();
+  l1.setCrossOrigin('Anonymous');
+  l1.load('https://s3-us-west-2.amazonaws.com/s.cdpn.io/175711/winter.jpg', function (img) {
+    slide.setImage(img);
+  });
   root.scene.add(slide);
 
   var slide2 = new Slide(width, height, 'in');
   var l2 = new THREE.ImageLoader();
-	l2.setCrossOrigin('Anonymous');
-	l2.load('../../../ressources/images/seasonBG.jpg', function(img) {
-		slide2.setImage(img);
-	})
-	
+  l2.setCrossOrigin('Anonymous');
+  l2.load('https://s3-us-west-2.amazonaws.com/s.cdpn.io/175711/spring.jpg', function (img) {
+    slide2.setImage(img);
+  });
+
   root.scene.add(slide2);
 
-  var tl = new TimelineMax({repeat:-1, repeatDelay:1.0, yoyo: true});
+  var tl = new TimelineMax({ repeat: -1, repeatDelay: 1.0, yoyo: true });
 
   tl.add(slide.transition(), 0);
   tl.add(slide2.transition(), 0);
 
   createTweenScrubber(tl);
 
-  window.addEventListener('keyup', function(e) {
+  window.addEventListener('keyup', function (e) {
     if (e.keyCode === 80) {
       tl.paused(!tl.paused());
     }
@@ -112,14 +113,14 @@ function Slide(width, height, animationPhase) {
     var delayY;
 
     if (animationPhase === 'in') {
-      delayY = THREE.Math.mapLinear(Math.abs(centroid.y), 0, height * 0.5, 0.0, maxDelayY)
-    }
-    else {
-      delayY = THREE.Math.mapLinear(Math.abs(centroid.y), 0, height * 0.5, maxDelayY, 0.0)
+      delayY = THREE.Math.mapLinear(Math.abs(centroid.y), 0, height * 0.5, 0.0, maxDelayY);
+    } else
+    {
+      delayY = THREE.Math.mapLinear(Math.abs(centroid.y), 0, height * 0.5, maxDelayY, 0.0);
     }
 
     for (v = 0; v < 6; v += 2) {
-      aAnimation.array[i2 + v]     = delayX + delayY + (Math.random() * stretch * duration);
+      aAnimation.array[i2 + v] = delayX + delayY + Math.random() * stretch * duration;
       aAnimation.array[i2 + v + 1] = duration;
     }
 
@@ -131,68 +132,68 @@ function Slide(width, height, animationPhase) {
     if (animationPhase === 'in') {
       control0.copy(centroid).sub(getControlPoint0(centroid));
       control1.copy(centroid).sub(getControlPoint1(centroid));
-    }
-    else { // out
+    } else
+    {// out
       control0.copy(centroid).add(getControlPoint0(centroid));
       control1.copy(centroid).add(getControlPoint1(centroid));
     }
 
     for (v = 0; v < 9; v += 3) {
-      aStartPosition.array[i3 + v]     = startPosition.x;
+      aStartPosition.array[i3 + v] = startPosition.x;
       aStartPosition.array[i3 + v + 1] = startPosition.y;
       aStartPosition.array[i3 + v + 2] = startPosition.z;
 
-      aControl0.array[i3 + v]     = control0.x;
+      aControl0.array[i3 + v] = control0.x;
       aControl0.array[i3 + v + 1] = control0.y;
       aControl0.array[i3 + v + 2] = control0.z;
 
-      aControl1.array[i3 + v]     = control1.x;
+      aControl1.array[i3 + v] = control1.x;
       aControl1.array[i3 + v + 1] = control1.y;
       aControl1.array[i3 + v + 2] = control1.z;
 
-      aEndPosition.array[i3 + v]     = endPosition.x;
+      aEndPosition.array[i3 + v] = endPosition.x;
       aEndPosition.array[i3 + v + 1] = endPosition.y;
       aEndPosition.array[i3 + v + 2] = endPosition.z;
     }
   }
 
   var material = new THREE.BAS.BasicAnimationMaterial(
-    {
-      shading: THREE.FlatShading,
-      side: THREE.DoubleSide,
-      uniforms: {
-        uTime: {type: 'f', value: 0}
-      },
-      shaderFunctions: [
-        THREE.BAS.ShaderChunk['cubic_bezier'],
-        //THREE.BAS.ShaderChunk[(animationPhase === 'in' ? 'ease_out_cubic' : 'ease_in_cubic')],
-        THREE.BAS.ShaderChunk['ease_in_out_cubic'],
-        THREE.BAS.ShaderChunk['quaternion_rotation']
-      ],
-      shaderParameters: [
-        'uniform float uTime;',
-        'attribute vec2 aAnimation;',
-        'attribute vec3 aStartPosition;',
-        'attribute vec3 aControl0;',
-        'attribute vec3 aControl1;',
-        'attribute vec3 aEndPosition;',
-      ],
-      shaderVertexInit: [
-        'float tDelay = aAnimation.x;',
-        'float tDuration = aAnimation.y;',
-        'float tTime = clamp(uTime - tDelay, 0.0, tDuration);',
-        'float tProgress = ease(tTime, 0.0, 1.0, tDuration);'
-        //'float tProgress = tTime / tDuration;'
-      ],
-      shaderTransformPosition: [
-        (animationPhase === 'in' ? 'transformed *= tProgress;' : 'transformed *= 1.0 - tProgress;'),
-        'transformed += cubicBezier(aStartPosition, aControl0, aControl1, aEndPosition, tProgress);'
-      ]
-    },
-    {
-      map: new THREE.Texture(),
-    }
-  );
+  {
+    shading: THREE.FlatShading,
+    side: THREE.DoubleSide,
+    uniforms: {
+      uTime: { type: 'f', value: 0 } },
+
+    shaderFunctions: [
+    THREE.BAS.ShaderChunk['cubic_bezier'],
+    //THREE.BAS.ShaderChunk[(animationPhase === 'in' ? 'ease_out_cubic' : 'ease_in_cubic')],
+    THREE.BAS.ShaderChunk['ease_in_out_cubic'],
+    THREE.BAS.ShaderChunk['quaternion_rotation']],
+
+    shaderParameters: [
+    'uniform float uTime;',
+    'attribute vec2 aAnimation;',
+    'attribute vec3 aStartPosition;',
+    'attribute vec3 aControl0;',
+    'attribute vec3 aControl1;',
+    'attribute vec3 aEndPosition;'],
+
+    shaderVertexInit: [
+    'float tDelay = aAnimation.x;',
+    'float tDuration = aAnimation.y;',
+    'float tTime = clamp(uTime - tDelay, 0.0, tDuration);',
+    'float tProgress = ease(tTime, 0.0, 1.0, tDuration);'
+    //'float tProgress = tTime / tDuration;'
+    ],
+    shaderTransformPosition: [
+    animationPhase === 'in' ? 'transformed *= tProgress;' : 'transformed *= 1.0 - tProgress;',
+    'transformed += cubicBezier(aStartPosition, aControl0, aControl1, aEndPosition, tProgress);'] },
+
+
+  {
+    map: new THREE.Texture() });
+
+
 
   THREE.Mesh.call(this, geometry, material);
 
@@ -206,16 +207,16 @@ Object.defineProperty(Slide.prototype, 'time', {
   },
   set: function (v) {
     this.material.uniforms['uTime'].value = v;
-  }
-});
+  } });
 
-Slide.prototype.setImage = function(image) {
+
+Slide.prototype.setImage = function (image) {
   this.material.uniforms.map.value.image = image;
   this.material.uniforms.map.value.needsUpdate = true;
 };
 
-Slide.prototype.transition = function() {
-  return TweenMax.fromTo(this, 3.0, {time:0.0}, {time:this.totalDuration, ease:Power0.easeInOut});
+Slide.prototype.transition = function () {
+  return TweenMax.fromTo(this, 3.0, { time: 0.0 }, { time: this.totalDuration, ease: Power0.easeInOut });
 };
 
 
@@ -235,15 +236,15 @@ SlideGeometry.prototype.bufferPositions = function () {
     var b = this.modelGeometry.vertices[face.b];
     var c = this.modelGeometry.vertices[face.c];
 
-    positionBuffer[face.a * 3]     = a.x - centroid.x;
+    positionBuffer[face.a * 3] = a.x - centroid.x;
     positionBuffer[face.a * 3 + 1] = a.y - centroid.y;
     positionBuffer[face.a * 3 + 2] = a.z - centroid.z;
 
-    positionBuffer[face.b * 3]     = b.x - centroid.x;
+    positionBuffer[face.b * 3] = b.x - centroid.x;
     positionBuffer[face.b * 3 + 1] = b.y - centroid.y;
     positionBuffer[face.b * 3 + 2] = b.z - centroid.z;
 
-    positionBuffer[face.c * 3]     = c.x - centroid.x;
+    positionBuffer[face.c * 3] = c.x - centroid.x;
     positionBuffer[face.c * 3 + 1] = c.y - centroid.y;
     positionBuffer[face.c * 3 + 2] = c.z - centroid.z;
   }
@@ -256,22 +257,22 @@ function THREERoot(params) {
     zNear: 10,
     zFar: 100000,
 
-    createCameraControls: true
-  }, params);
+    createCameraControls: true },
+  params);
 
   this.renderer = new THREE.WebGLRenderer({
     antialias: params.antialias,
-    alpha: true
-  });
+    alpha: true });
+
   this.renderer.setPixelRatio(Math.min(2, window.devicePixelRatio || 1));
   document.getElementById('three-container').appendChild(this.renderer.domElement);
 
   this.camera = new THREE.PerspectiveCamera(
-    params.fov,
-    window.innerWidth / window.innerHeight,
-    params.zNear,
-    params.zfar
-  );
+  params.fov,
+  window.innerWidth / window.innerHeight,
+  params.zNear,
+  params.zfar);
+
 
   this.scene = new THREE.Scene();
 
@@ -304,8 +305,8 @@ THREERoot.prototype = {
     this.camera.updateProjectionMatrix();
 
     this.renderer.setSize(window.innerWidth, window.innerHeight);
-  }
-};
+  } };
+
 
 ////////////////////
 // UTILS
@@ -325,15 +326,15 @@ var utils = {
   ease: function (ease, t, b, c, d) {
     return b + ease.getRatio(t / d) * c;
   },
-  fibSpherePoint: (function () {
-    var vec = {x: 0, y: 0, z: 0};
+  fibSpherePoint: function () {
+    var vec = { x: 0, y: 0, z: 0 };
     var G = Math.PI * (3 - Math.sqrt(5));
 
     return function (i, n, radius) {
       var step = 2.0 / n;
       var r, phi;
 
-      vec.y = i * step - 1 + (step * 0.5);
+      vec.y = i * step - 1 + step * 0.5;
       r = Math.sqrt(1 - vec.y * vec.y);
       phi = i * G;
       vec.x = Math.cos(phi) * r;
@@ -346,9 +347,9 @@ var utils = {
       vec.z *= radius;
 
       return vec;
-    }
-  })(),
-  spherePoint: (function () {
+    };
+  }(),
+  spherePoint: function () {
     return function (u, v) {
       u === undefined && (u = Math.random());
       v === undefined && (v = Math.random());
@@ -357,29 +358,29 @@ var utils = {
       var phi = Math.acos(2 * v - 1);
 
       var vec = {};
-      vec.x = (Math.sin(phi) * Math.cos(theta));
-      vec.y = (Math.sin(phi) * Math.sin(theta));
-      vec.z = (Math.cos(phi));
+      vec.x = Math.sin(phi) * Math.cos(theta);
+      vec.y = Math.sin(phi) * Math.sin(theta);
+      vec.z = Math.cos(phi);
 
       return vec;
-    }
-  })()
-};
+    };
+  }() };
+
 
 function createTweenScrubber(tween, seekSpeed) {
   seekSpeed = seekSpeed || 0.001;
 
   function stop() {
-    TweenMax.to(tween, 1, {timeScale:0});
+    TweenMax.to(tween, 1, { timeScale: 0 });
   }
 
   function resume() {
-    TweenMax.to(tween, 1, {timeScale:1});
+    TweenMax.to(tween, 1, { timeScale: 1 });
   }
 
   function seek(dx) {
     var progress = tween.progress();
-    var p = THREE.Math.clamp((progress + (dx * seekSpeed)), 0, 1);
+    var p = THREE.Math.clamp(progress + dx * seekSpeed, 0, 1);
 
     tween.progress(p);
   }
@@ -390,18 +391,18 @@ function createTweenScrubber(tween, seekSpeed) {
   var mouseDown = false;
   document.body.style.cursor = 'pointer';
 
-  window.addEventListener('mousedown', function(e) {
+  window.addEventListener('mousedown', function (e) {
     mouseDown = true;
     document.body.style.cursor = 'ew-resize';
     _cx = e.clientX;
     stop();
   });
-  window.addEventListener('mouseup', function(e) {
+  window.addEventListener('mouseup', function (e) {
     mouseDown = false;
     document.body.style.cursor = 'pointer';
     resume();
   });
-  window.addEventListener('mousemove', function(e) {
+  window.addEventListener('mousemove', function (e) {
     if (mouseDown === true) {
       var cx = e.clientX;
       var dx = cx - _cx;
@@ -411,16 +412,16 @@ function createTweenScrubber(tween, seekSpeed) {
     }
   });
   // mobile
-  window.addEventListener('touchstart', function(e) {
+  window.addEventListener('touchstart', function (e) {
     _cx = e.touches[0].clientX;
     stop();
     e.preventDefault();
   });
-  window.addEventListener('touchend', function(e) {
+  window.addEventListener('touchend', function (e) {
     resume();
     e.preventDefault();
   });
-  window.addEventListener('touchmove', function(e) {
+  window.addEventListener('touchmove', function (e) {
     var cx = e.touches[0].clientX;
     var dx = cx - _cx;
     _cx = cx;
@@ -429,3 +430,4 @@ function createTweenScrubber(tween, seekSpeed) {
     e.preventDefault();
   });
 }
+    </script>
