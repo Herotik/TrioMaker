78822e488e494a0d010ab99d763e219bbee0231a
Herotik <alex.mks6@gmail.com>
2019-09-25 11:50:34 +0200

    ajout page test

--- assets/js/three.js
+++ assets/js/three.js
@@ -1,5 +1,5 @@
 //JS
-$("body").append($("<script src='https://https://cdnjs.cloudflare.com/ajax/libs/three.js/r79/three.min.js'></script>"))
+
 require('../../public/assets/three/js/TweenMax.min.js');
 require('../../public/assets/three/js/bas.js');
 require('../../public/assets/three/js/OrbitControls-2.js');
